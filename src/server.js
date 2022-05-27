22dd02c71c3a3b142ef6aa22fcd2e4a9f9a90d41
Amanda Guerra <guerraamanda1@hotmail.com>
2022-05-27 10:39:18 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+//importação do express
+
+const routes = require("./routes")
+
+const app = express()
+app.use(json())
+//inicialização do express
+
+app.use(routes)
+
+const PORT = 3344
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+//configuração da porta para o express
