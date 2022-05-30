297c1f8a25d848cc0514f2594004f61ce4af1c8d
Amanda Guerra <guerraamanda1@hotmail.com>
2022-05-30 17:05:45 -0300

    Automação da criação das tabelas no sqlite

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -7,7 +7,7 @@ async function sqliteConnection() {
     filename: path.resolve(__dirname, "..", "database.db"),
     driver: sqlite3.Database
   })
-  return
+  return database
 }
 
 module.exports = sqliteConnection
